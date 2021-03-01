120967277183e0f4ffa688a7deffb1c0617ca1c2
Jessica Fernandes <jessicafernandes.055@gmail.com>
2021-03-01 01:55:57 -0300

    update movie details

--- app/src/main/java/com/br/matchmovies/model/Movie.kt
+++ app/src/main/java/com/br/matchmovies/model/Movie.kt
@@ -1,4 +1,5 @@
 package com.br.matchmovies.model
 
-data class Movie (val title: String, val overview: String, val rating: Float, val imageMovie: Int) {
+data class Movie (val title: String, val overview: String, val rating: Float, val genres: String,
+                  val cast: String, val director: String, val imageMovie: Int) {
 }
--- app/src/main/java/com/br/matchmovies/view/MovieDetailsActivity.kt
+++ app/src/main/java/com/br/matchmovies/view/MovieDetailsActivity.kt
@@ -16,24 +16,33 @@ class MovieDetailsActivity : AppCompatActivity() {
     private val ratingBar by lazy { findViewById<RatingBar>(R.id.rating) }
     private val btnTrailer by lazy { findViewById<Button>(R.id.btn_trailer) }
     private val btnMatch by lazy { findViewById<Button>(R.id.btn_match) }
-    private val btnBack by lazy { findViewById<Button>(R.id.btn_back) }
+    private val btnEvaluate by lazy { findViewById<Button>(R.id.btn_evaluate) }
+    private val btnShare by lazy { findViewById<Button>(R.id.btn_share) }
+    private val genre by lazy { findViewById<TextView>(R.id.tv_genres) }
+    private val director by lazy { findViewById<TextView>(R.id.tv_director) }
+    private val cast by lazy { findViewById<TextView>(R.id.tv_cast) }
     private val textOverview by lazy { findViewById<TextView>(R.id.tv_overview) }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        initViews()
 
-        val movie = Movie("Annabelle (2014)", "Em Anabelle, John (Ward Horton) encontrou o presente perfeito para sua futura esposa, Mia (Annabelle Wallis): uma rara boneca antiga com um lindo vestido. Mia fica bastante contente com seu presente, porém, sua animação com a boneca não duraria muito tempo.",
-            3.0F, R.drawable.anabele)
-        initViews(movie)
     }
 
-    private fun initViews(movie: Movie) {
+    private fun initViews() {
+
+        val movie = Movie("Annabelle (2014)", "Em Anabelle, John (Ward Horton) encontrou o presente perfeito para sua futura esposa, Mia (Annabelle Wallis): uma rara boneca antiga com um lindo vestido. Mia fica bastante contente com seu presente, porém, sua animação com a boneca não duraria muito tempo.",
+                3.0F, "Gênero: Terror", "Elenco:  Ward Horton, Annabelle Wallis, Alfre Woodard","Direção: John R. Leonetti", R.drawable.anabele)
+
         imageMovie.setImageResource(movie.imageMovie)
         title.text = movie.title
         ratingBar.rating = movie.rating
         textOverview.text = movie.overview
+        genre.text = movie.genres
+        director.text = movie.director
+        cast.text = movie.cast
 
 
     }
