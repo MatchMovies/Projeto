aa5c6037fa1b614fd70120ff8f3b53f884cb4382
ederbueno <bueno.ederson@gmail.com>
2021-03-22 21:06:19 -0300

    bottom

---
+++ app/src/main/java/com/br/matchmovies/fragments/ProfileFragment.kt
@@ -0,0 +1,25 @@
+package com.br.matchmovies.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.br.matchmovies.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class ProfileFragment : Fragment() {
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?)
+         : View? {
+        // Inflate the layout for this fragment
+       return inflater.inflate(R.layout.fragment_profile, container, false)
+
+
+    }
+
+}
--- app/src/main/java/com/br/matchmovies/view/HomeActivity.kt
+++ app/src/main/java/com/br/matchmovies/view/HomeActivity.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import com.br.matchmovies.R
 import com.br.matchmovies.fragments.HomeFragment
 import com.br.matchmovies.fragments.MatchFragment
+import com.br.matchmovies.fragments.MovieFragment
+import com.br.matchmovies.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
@@ -18,24 +20,23 @@ class HomeActivity : AppCompatActivity() {
 
          val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navegation)
 
+
         makeCurrentFragment(HomeFragment())
         bottomNavigationView.setOnNavigationItemReselectedListener {
             when(it.itemId){
                 R.id.ic_home_nav_bar -> makeCurrentFragment(HomeFragment())
                 R.id.ic_match_nav_bar -> makeCurrentFragment(MatchFragment())
+                R.id.ic_movie_nav_bar -> makeCurrentFragment(MovieFragment())
+                R.id.ic_profile_nav_bar -> makeCurrentFragment(ProfileFragment())
             }
             true
         }
 
         }
 
-
     private fun makeCurrentFragment(fragment: Fragment) =
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.fl_wrapper, fragment)
-                commit()
+                commitNow()
             }
-
-
-
 }
