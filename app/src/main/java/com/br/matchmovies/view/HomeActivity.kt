4f97ae1ccadccbe78466ed1c937e56b0b6f47879
ederbueno <bueno.ederson@gmail.com>
2021-03-24 00:20:50 -0300

    merge

--- app/src/main/java/com/br/matchmovies/view/HomeActivity.kt
+++ app/src/main/java/com/br/matchmovies/view/HomeActivity.kt
@@ -4,22 +4,11 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.br.matchmovies.R
 import com.br.matchmovies.fragments.HomeFragment
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.br.matchmovies.R
-import com.br.matchmovies.fragments.HomeFragment
 import com.br.matchmovies.fragments.MatchFragment
 import com.br.matchmovies.fragments.MovieFragment
 import com.br.matchmovies.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.br.matchmovies.R
-import com.br.matchmovies.fragments.HomeFragment
-import com.br.matchmovies.fragments.MatchFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 
 
@@ -31,27 +20,23 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navegation)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navegation)
 
         makeCurrentFragment(HomeFragment())
         bottomNavigationView.setOnNavigationItemReselectedListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.ic_home_nav_bar -> makeCurrentFragment(HomeFragment())
                 R.id.ic_match_nav_bar -> makeCurrentFragment(MatchFragment())
                 R.id.ic_movie_nav_bar -> makeCurrentFragment(MovieFragment())
                 R.id.ic_profile_nav_bar -> makeCurrentFragment(ProfileFragment())
             }
-          true      
+            true
         }
-        
+    }
 
-    private fun makeCurrentFragment(fragment: Fragment) =
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fl_wrapper, fragment)
-                commitNow()
-            }
-
-
-
-
+    private  fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commitNow()
+        }
 }
--- app/src/main/java/com/br/matchmovies/view/PerfilActivity.kt
+++ app/src/main/java/com/br/matchmovies/view/PerfilActivity.kt
@@ -1,8 +1,4 @@
 package com.br.matchmovies.view
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.br.matchmovies.R
