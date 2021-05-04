0e54eea92474d04a9f44c4e06c39ee67b837a21d
Filipecarmoss <filipe.carmoss@hotmail.com>
2021-05-03 21:44:45 -0300

    Atualização Tela Login e Cadastro usuário FireBase

--- app/src/main/java/com/br/matchmovies/view/CadastroActivity.kt
+++ app/src/main/java/com/br/matchmovies/view/CadastroActivity.kt
@@ -2,10 +2,15 @@ package com.br.matchmovies.view
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.br.matchmovies.R
 import com.br.matchmovies.databinding.ActivityMainCadastroBinding
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class CadastroActivity : AppCompatActivity() {
 
@@ -17,6 +22,52 @@ class CadastroActivity : AppCompatActivity() {
         binding = ActivityMainCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+        binding.btcadastro.setOnClickListener {
+
+            val nome = binding.editNome.text.toString()
+            val tel = binding.editNumerotelefone.text.toString()
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+            val mensagem_erro = binding.mensagemErro
+
+            if (nome.isEmpty() || tel.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                mensagem_erro.setText("Preencha todos os campos")
+            }else{
+                CadastrarUsuario()
+            }
+        }
+
+    }
+
+    private fun CadastrarUsuario(){
+
+        val nome = binding.editNome.text.toString()
+        val tel = binding.editNumerotelefone.text.toString()
+        val email = binding.editEmail.text.toString()
+        val senha = binding.editSenha.text.toString()
+        val mensagem_erro = binding.mensagemErro
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,senha).addOnCompleteListener {
+            if (it.isSuccessful){
+                Toast.makeText(this, "Usuário cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+                binding.editEmail.setText("")
+                binding.editSenha.setText("")
+                binding.editNome.setText("")
+                binding.editNumerotelefone.setText("")
+                mensagem_erro.setText("")
+            }
+        }.addOnFailureListener {
+
+            var erro = it
+            when{
+
+                erro is FirebaseAuthWeakPasswordException -> mensagem_erro.setText("Digite uma senha com no mínimo 6 caracteres")
+                erro is FirebaseAuthUserCollisionException -> mensagem_erro.setText("E-mail já cadastrado")
+                erro is FirebaseNetworkException -> mensagem_erro.setText("Sem conexão com a internet")
+                else ->  mensagem_erro.setText("Erro ao cadastrar Usuário")
+            }
+
+        }
+
     }
 }
